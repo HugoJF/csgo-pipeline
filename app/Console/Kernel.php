98dcae3f85f921d372d904b61b12c6e3a39399da
Hugo Jeller <hugo_jeller@hotmail.com>
2019-05-17 22:01:32 -0400

    fixes command being deployed instantly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-    	$schedule->job(new ProcessEvents());
+    	$schedule->job(new ProcessEvents())->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
