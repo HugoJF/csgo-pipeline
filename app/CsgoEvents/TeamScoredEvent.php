0b18d9f11265751dc97029709a01a01dcd3df532
Hugo Jeller <hugo_jeller@hotmail.com>
2019-05-21 01:10:43 -0400

    another missing delimiter

--- app/CsgoEvents/TeamScoredEvent.php
+++ app/CsgoEvents/TeamScoredEvent.php
@@ -14,7 +14,7 @@ use App\User;
 
 class TeamScoredEvent extends CsgoEvent implements \JsonSerializable
 {
-	private const PATTERN = "(\d{1,2}\/\d{1,2}\/\d{1,4})\s-\s(\d{1,2}:\d{1,2}:\d{1,2}):\sTeam\s\"([A-Za-z]*?)\"\sscored\s\"(\d{1,5})\"\swith\s\"(\d{1,5})\"\splayers";
+	private const PATTERN = "/(\d{1,2}\/\d{1,2}\/\d{1,4})\s-\s(\d{1,2}:\d{1,2}:\d{1,2}):\sTeam\s\"([A-Za-z]*?)\"\sscored\s\"(\d{1,5})\"\swith\s\"(\d{1,5})\"\splayers/i";
 
 	public $date;
 	public $time;
