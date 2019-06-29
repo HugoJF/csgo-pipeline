beffbabab6ff9f850d6b2952e4c67050b5e9a861
vagrant <vagrant@homestead>
2019-06-29 01:10:42 -0400

    added pending pipe to menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('servers.create') }}">Create server</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('pipes.view-by-key', 'pending') }}">Pending events</a>
+                    </li>
                 </ul>
                 
                 <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('pipes')->name('pipes.')->group(function () {
 	Route::get('create', 'PipeController@create')->name('create');
-	Route::get('by-key/{key}', 'PipeController@viewByKey')->name('view');
+	Route::get('by-key/{key}', 'PipeController@viewByKey')->name('view-by-key');
 	Route::get('{pipe}', 'PipeController@view')->name('view');
 	Route::get('{pipe}/edit', 'PipeController@edit')->name('edit');
 
